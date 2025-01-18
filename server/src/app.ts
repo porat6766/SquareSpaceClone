8c235a3e480eaf2f2226e5fc608cef0903b649bc
porat6766 <porat850@gmail.com>
2025-01-19 01:28:52 +0200

    pp

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(
   cors({
