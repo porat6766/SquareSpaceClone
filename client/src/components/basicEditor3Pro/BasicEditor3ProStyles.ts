77554951bca7e013143c89dad645a670b2eee851
porat6766 <porat850@gmail.com>
2025-01-19 22:39:26 +0200

    p

--- client/src/components/basicEditor3Pro/BasicEditor3ProStyles.ts
+++ client/src/components/basicEditor3Pro/BasicEditor3ProStyles.ts
@@ -1,7 +1,7 @@
-import { display } from "html2canvas/dist/types/css/property-descriptors/display";
-import { textAlign } from "html2canvas/dist/types/css/property-descriptors/text-align";
-import { color } from "html2canvas/dist/types/css/types/color";
-import { AlignJustify } from "lucide-react";
+// import { display } from "html2canvas/dist/types/css/property-descriptors/display";
+// import { textAlign } from "html2canvas/dist/types/css/property-descriptors/text-align";
+// import { color } from "html2canvas/dist/types/css/types/color";
+// import { AlignJustify } from "lucide-react";
 
 const styles = {
   default_red_rectangle_style: {
@@ -37,6 +37,18 @@ const styles = {
     textAlign: "center",
     width: "100%",
     height: "100%",
+  },
+  default_Button_Style: {
+    padding: "12px 24px",
+    backgroundColor: "#4CAF50",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "16px",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+    outline: "none",
+    width: "auto",
   },
 };
 
