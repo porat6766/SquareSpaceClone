58dd5886ab90e7a4a1a8101895992c56f2ce9ea7
eden shabi <eden9876102@gmail.com>
2025-01-21 16:14:07 +0200

    sm

--- client/src/pages/LogoMaker.tsx
+++ client/src/pages/LogoMaker.tsx
@@ -1,26 +1,36 @@
 import { useNavigate } from "react-router-dom";
 import Icon from "../assets/icon-squarespace.svg";
-import Banner from "../assets/splash-businesscard.jpg";
+import Banner from "../assets/splash-businesscard-copy.png";
 
 function LogoMaker() {
   const navigate = useNavigate();
 
   return (
-    <div className="relative flex flex-col justify-end items-center bg-white h-screen">
+    <div className=" bg-white h-screen w-screen">
       <span
         className="absolute left-10 top-8 cursor-pointer"
         onClick={() => navigate("/templates")}
       >
         <img src={Icon} alt="logo" className="max-w-8 max-h-8 opacity-40" />
       </span>
-      <div className="h-full z-10 -mb-96 flex flex-col items-center justify-end">
-        <h1 className="text-black text-8xl font-bold">Create your Logo</h1>
-        <p className="opacity-60 -mb-10 mt-10">
-          Enter your company name below to get started with our logo maker.
-        </p>
-      </div>
-      <div className="bottom-0">
-        <img src={Banner} alt="banner" className="h-full w-auto" />
+      <div className="flex flex-col justify-center items-center h-screen w-screen">
+        <div className="h-auto z-10 flex flex-col justify-center items-center">
+          <h1 className=" text-black text-7xl font-bold">Create your Logo</h1>
+          <p className=" opacity-60 mt-10">
+            Enter your company name below to get started with our logo maker.
+          </p>
+        </div>
+        <div className="relative bottom-0">
+          <input
+            placeholder="company name"
+            className="absolute top-64 right-1/3 mr-10 p-1 pl-3 w-96 bg-black text-white z-10 placeholder:opacity-60"
+          ></input>
+          <img
+            src={Banner}
+            alt="banner"
+            className="relative -bottom-4 -left-[300px] h-auto w-auto"
+          />
+        </div>
       </div>
     </div>
   );
