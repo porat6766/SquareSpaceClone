7690e6eec116fb92a25a15372a9e9996f9cab462
porat6766 <porat850@gmail.com>
2025-02-06 13:55:15 +0200

    Google auth

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,14 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import path from "path";
-
+import session from 'express-session';
+import passport from 'passport';
 import userRoutes from "./routes/userRoutes";
 import siteRoutes from "./routes/siteRoutes";
+import googleAuth from "./GoogleAuth/GoogleAuth";
+import crypto from "crypto"
 
 dotenv.config();
 
@@ -19,8 +22,27 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://squarespaceclone.onrender.com"],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
+
+const secret = crypto.randomBytes(64).toString('hex');
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -33,12 +55,15 @@ if (process.env.URI) {
   console.error("DB_URI environment variable is not defined");
 }
 
-app.get("/", (req: Request, res: Response): void => {
-  res.status(200).send({ message: "Server is alive !" });
-});
-
+app.use(googleAuth);
 app.use("/api/users", userRoutes);
 app.use("/api/sites", siteRoutes);
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send({ error: err.message });
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
